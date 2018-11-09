53a5f4d7941ba99703585f579b8d40e33c5a9e09
manohiro <manohiro0@gmail.com>
2018-11-09 17:09:03 +0900

    プログラム一部修正

--- ml-scratch/SVM_model/SVM.py
+++ ml-scratch/SVM_model/SVM.py
@@ -1,6 +1,4 @@
-import numpy as np
-
-class ScratchSVM:
+class SVM_test:
     def __init__(self, num_iter, lr):
         self.iter = num_iter
         self.lr = lr
@@ -10,11 +8,10 @@ class ScratchSVM:
 
     def fit(self, X, y):
         # ラグランジュ係数
-        a = np.random.randn(len(X), 1)
-        # イテレータの回数分回す
+        a = np.random.rand(len(X), 1)
+        
+        # 各ラベルのサポートベクターが見つかるまでループ
         for num in range(self.iter):
-            flag1 = False # サポートベクタラベル1用 検出フラグ
-            flag0 = False # サポートベクタラベル-1用 検出フラグ
             # データ数分ループ
             for i in range(len(X)):
                 temp = 0
@@ -22,41 +19,19 @@ class ScratchSVM:
                     temp += a[j]*y[i]*y[j]*np.dot(X[i].T, X[j])
                 # ラグランジュ係数更新式
                 a[i] = a[i] + self.lr * (1 - temp)
-                # サポートベクター判定
-                if(a[i] >= 1e-5):
-                    if( y[i] == 1):
-                        if(not flag1):
-                            # ラベル1用サポートベクター
-                            sv_lt_1 = X[i]    # ラベル1のサポートベクター
-                            a_sv_1 = a[i]     # ラベル1のラグランジュ係数
-                            flag1 = True
-                        else:
-                            sv_lt_1 = np.vstack((sv_lt_1, X[i])) # ラベル1のサポートベクター
-                            a_sv_1 = np.vstack((a_sv_1, a[i]))   # ラベル1のラグランジュ係数
-                    else:
-                        if(not flag0):
-                            # ラベル-1用サポートベクター
-                            sv_lt_0 = X[i]     # ラベル-1のサポートベクター
-                            a_sv_0 = a[i]      # ラベル-1のラグランジュ係数
-                            flag0 = True  
-                        else:
-                            sv_lt_0 = np.vstack((sv_lt_0, X[i])) # ラベル-1のサポートベクター
-                            a_sv_0 = np.vstack((a_sv_0, a[i]))   # ラベル-1のラグランジュ係数
                 # ラグランジュ係数がマイナスの場合
                 if(a[i] < 0):
                     a[i] = 0
-            # 各ラベルのサポートベクター取得判定
-            if(flag1 and flag0):
-                # ラベル
-                self.y_label = np.vstack((np.full((len(sv_lt_1), 1),1), np.full((len(sv_lt_0),1) , -1)))
-                # サポートベクター
-                self.sv_lt = np.vstack((sv_lt_1, sv_lt_0))
-                # ラグランジュ係数
-                self.a_sv = np.vstack((a_sv_1, a_sv_0))
-                
+        # サポートベクター判定
+        self.a_sv = a[a >= 1e-5]
+        index_sv = np.where(a >= 1e-5)[0]
+        print(index_sv)
+        self.y_label = y[index_sv]
+        self.sv_lt = X[index_sv]       
+        
     def predict(self, X):
         # 推定
         f_x = 0
         for i in range(len(self.sv_lt)):
-            f_x = f_x + self.a_sv[i] * self.y_label[i] * np.dot(X, self.sv_lt[i].T)
+                f_x = f_x + self.a_sv[i] * self.y_label[i] * np.dot(X, self.sv_lt[i].T)
         return f_x
